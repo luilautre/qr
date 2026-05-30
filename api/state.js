6ed7f3d0b61a01353d8678678cfff4cfd1826362
luilautre <achillebou13@gmail.com>
2026-05-30 17:26:27 +0200

    Update state.js

--- api/state.js
+++ api/state.js
@@ -6,12 +6,4 @@ export default function handler(req, res) {
     res.json({
         visible: !scannedRecently
     });
-}import { kv } from "@vercel/kv";
-
-export default async function handler(req, res) {
-  const hidden = await kv.get("qrHidden");
-
-  res.status(200).json({
-    visible: !hidden
-  });
 }
