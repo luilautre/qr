2a0b72c3b5644325f1243db62ff250ed92564004
luilautre <achillebou13@gmail.com>
2026-05-30 19:16:07 +0200

    Update qr.js

--- api/qr.js
+++ api/qr.js
@@ -2,24 +2,18 @@ import QRCode from "qrcode";
 
 export default async function handler(req, res) {
     try {
-        const url = "https://qrllt.vercel.app/api/scan";
+        const url = `${req.headers["x-forwarded-proto"] || "https"}://${req.headers.host}/api/scan`;
 
-        const qr = await QRCode.toDataURL(url);
+        const buffer = await QRCode.toBuffer(url, {
+            width: 300,
+            margin: 2
+        });
 
-        res.status(200).json({ qr });
+        res.setHeader("Content-Type", "image/png");
+        res.status(200).send(buffer);
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({
+            error: err.message
+        });
     }
-}api/qr.jsimport QRCode from "qrcode";
-
-export default async function handler(req, res) {
-    const url = `${req.headers["x-forwarded-proto"] || "https"}://${req.headers.host}/api/scan`;
-
-    const buffer = await QRCode.toBuffer(url, {
-        width: 300,
-        margin: 2
-    });
-
-    res.setHeader("Content-Type", "image/png");
-    res.status(200).send(buffer);
 }
