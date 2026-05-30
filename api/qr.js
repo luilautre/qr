de6c12d0c71025c38e8d145de0c0d25a58245dd7
luilautre <achillebou13@gmail.com>
2026-05-30 19:14:32 +0200

    Update qr.js

--- api/qr.js
+++ api/qr.js
@@ -1,4 +1,16 @@
-api/qr.jsimport QRCode from "qrcode";
+import QRCode from "qrcode";
+
+export default async function handler(req, res) {
+    try {
+        const url = "https://qrllt.vercel.app/api/scan";
+
+        const qr = await QRCode.toDataURL(url);
+
+        res.status(200).json({ qr });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}api/qr.jsimport QRCode from "qrcode";
 
 export default async function handler(req, res) {
     const url = `${req.headers["x-forwarded-proto"] || "https"}://${req.headers.host}/api/scan`;
