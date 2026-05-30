5f944fe3d1c98b084584776d9c32d513121b3d8d
luilautre <achillebou13@gmail.com>
2026-05-30 19:17:54 +0200

    Update scan.js

--- api/scan.js
+++ api/scan.js
@@ -1,17 +1,4 @@
-import { notifyScan } from "./events.js";
-
 export default function handler(req, res) {
-    notifyScan();
-
-    res.writeHead(302, {
-        Location: "https://google.com"
-    });
-
-    res.end();
-}let lastScan = 0;
-
-export default function handler(req, res) {
-    lastScan = Date.now();
-    global.lastScan = lastScan;
+    global.lastScan = Date.now();
     res.redirect("/");
 }
