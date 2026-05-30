bb1abce4fa3f7b66c2ba027b70c095215447aa6f
luilautre <achillebou13@gmail.com>
2026-05-30 19:09:56 +0200

    Update scan.js

--- api/scan.js
+++ api/scan.js
@@ -1,4 +1,14 @@
-let lastScan = 0;
+import { notifyScan } from "./events.js";
+
+export default function handler(req, res) {
+    notifyScan();
+
+    res.writeHead(302, {
+        Location: "https://google.com"
+    });
+
+    res.end();
+}let lastScan = 0;
 
 export default function handler(req, res) {
     lastScan = Date.now();
