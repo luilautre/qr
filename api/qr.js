719f7bf2cd4979f823ecd5f23556499bffe704c5
luilautre <achillebou13@gmail.com>
2026-05-30 17:10:10 +0200

    Create qr.js

---
+++ api/qr.js
@@ -0,0 +1,13 @@
+import QRCode from "qrcode";
+
+export default async function handler(req, res) {
+    const url = `${req.headers["x-forwarded-proto"] || "https"}://${req.headers.host}/api/scan`;
+
+    const buffer = await QRCode.toBuffer(url, {
+        width: 300,
+        margin: 2
+    });
+
+    res.setHeader("Content-Type", "image/png");
+    res.status(200).send(buffer);
+}
