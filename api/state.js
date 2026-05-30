c0513021b68017ab58c7a03fb7def7c5d97a8bc3
luilautre <achillebou13@gmail.com>
2026-05-30 21:27:12 +0200

    Update state.js

--- api/state.js
+++ api/state.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
     res.json({
         lastScan: global.lastScan || null,
-        now: Date.now()
+        now: Date.now(),
+        visible: !(global.lastScan && Date.now() - global.lastScan < 5000)
     });
 }
