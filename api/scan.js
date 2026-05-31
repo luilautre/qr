e42d73763c2af9d1f264a3aefadc0cbb902474cb
luilautre <achillebou13@gmail.com>
2026-05-31 09:24:37 +0200

    gg

--- api/scan.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-    global.lastScan = Date.now();
-    res.redirect(307, "/");
-}
--- api/state.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    res.json({
-        lastScan: global.lastScan || null,
-        now: Date.now(),
-        visible: !(global.lastScan && Date.now() - global.lastScan < 5000)
-    });
-}
